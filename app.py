5678f713782bb7051548fd869037322cbfc3901f
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-12 12:11:39 +0900

    스트림릿 기본 템플릿

---
+++ app.py
@@ -0,0 +1,16 @@
+# 스트림릿 라이브러리를 사용하기 위한 임포트문 작성
+
+import streamlit as st
+
+# 웹 대시보드 프레임워크인, 스트림릿은
+# main 함수로 있어야 한다.
+
+def main() :
+    st.title('헬로우')
+    st. title('개발 프로젝트')
+    
+
+
+if __name__ == '__main__' :
+    main()
+
